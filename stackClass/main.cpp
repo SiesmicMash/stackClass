95717b9fdae153bbd222950068aac67b7537096c
SiesmicMash <usaidsyedali@gmail.com>
2025-02-10 08:44:49 -0800

    Initial Commit

---
+++ stackClass/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  stackClass
+//
+//  Created by Usaid Ali Syed on 2025-02-10.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
